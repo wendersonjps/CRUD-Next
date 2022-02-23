adaeb9d59f9ca91699e05f60e00d79ddf9cc1842
Wenderson Júnio <wendersonjps@gmail.com>
2022-02-23 13:31:28 -0300

    :wrench: Configuration: NextJS, TailwindCSS and Firebase

---
+++ crud-next/src/pages/index.tsx
@@ -0,0 +1,14 @@
+export default function Home() {
+  return (
+    <div className={`
+    flex 
+    h-screen 
+    justify-center 
+    items-center
+    bg-gradient-to-r from-blue-700 to-pink-500
+    `}>
+
+    <span className="text-4xl">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Mollitia, quod, dolore repellat cumque maiores aut iure magnam corrupti neque sequi velit nostrum nobis quidem pariatur, totam rerum nemo. Ipsa, harum.</span>
+    </div>
+  )
+}
